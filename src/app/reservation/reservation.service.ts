59618b13b2179a40a138e68c5f69b5803e39215b
Lenny BONDOUX <l.bondoux@lyon.ort.asso.fr>
2024-11-04 12:49:53 +0100

    45-46

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  private reservation: Reservation[] = [];
+
+  getAllReservations(): Reservation[] {
+      return this.reservation;
+  }
+
+  getReservation(id: string): Reservation | undefined {
+      return this.reservation.find(res => res.id === id);
+  }
+
+  addReservation(reservation: Reservation): void {
+      this.reservation.push(reservation);
+  }
+
+  deleteReservation(id: string): void {
+    let index = this.reservation.findIndex(res => res.id === id);
+    this.reservation.splice(index, 1);
+  }
+
+  editReservation(updatedReservation: Reservation): void {
+    let index = this.reservation.findIndex(res => res.id === updatedReservation.id);
+    this.reservation[index] = updatedReservation;
+  }
 }
